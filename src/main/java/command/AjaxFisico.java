3fc2da066a21f8307832bd72bf9dfb5a8e808697
michelnevess <michelneves0@gmail.com>
2015-11-20 11:09:16 -0200

    choro

--- src/main/java/command/AjaxFisico.java
+++ src/main/java/command/AjaxFisico.java
@@ -8,41 +8,50 @@ package command;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import database.ClienteDAO;
+import database.ServicoDAO;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Cliente;
-import model.ClienteList;
+import model.Servico;
 import spark.Request;
 import spark.Response;
 
 public class AjaxFisico extends Command {
 
     private String resposta;
-    private final String respostaXML;
+    
 
     public AjaxFisico(Request request, Response response) throws SQLException {
         super(request, response);
-        XStream xstream = new XStream();
-        xstream.alias("cliente", Cliente.class);
-        xstream.alias("clientes", ClienteList.class);
-        xstream.addImplicitCollection(ClienteList.class, "list");
-
-        ClienteList list = new ClienteList();
-        list.setList(new ClienteDAO().select());
+        
+        
+        String descricao = "";
+        String valor = "";
+        String d_inicio = "";
+        String d_fim = "";
+        String cliente_nome = "";
+        String funcionario = "";
        
-        this.respostaXML = xstream.toXML(list);
+               ArrayList<Servico> servico = new ServicoDAO().filtro(request.queryParams(":valor"));
+               for(int i = 0; i<servico.size(); i++){
+                   descricao+= "\"" + servico.get(i).getDescricao() + "\" ";
+                   valor+= "\"" + servico.get(i).getValor() + "\" ";
+                   d_inicio+= "\"" + servico.get(i).getD_inicio() + "\" ";
+                   d_fim+= "\"" + servico.get(i).getD_fim() + "\" ";
+                   cliente_nome+= "\"" + servico.get(i).getCliente().getNome() + "\" ";
+                   funcionario+= "\"" + servico.get(i).getFuncionario().getNome() + "\" ";
+               }
+               this.resposta = "[" + descricao.trim().replace(' ', ',') + "]>"+"[" + valor.trim().replace(' ', ',') + "]>"+
+                       "[" + d_inicio.trim().replace(' ', ',') + "]>"+"[" + d_fim.trim().replace(' ', ',') + "]>"+
+                       "[" + cliente_nome.trim().replace(' ', ',') + "]>"+"[" + funcionario.trim().replace(' ', ',') + "]>";
 
     }
 
     public String getResposta() {
         return this.resposta;
-    }
-
-    public String getRepostaXML() {
-        return this.respostaXML;
     }
 
 }
